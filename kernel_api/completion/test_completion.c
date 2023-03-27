ffc43317be68a9cf129ab661c96341079bd0f640
IotaHydrae <writeforever@foxmail.com>
2023-03-27 18:34:13 +0800

    starting add completion usage driver
    Signed-off-by: IotaHydrae <writeforever@foxmail.com>

---
+++ kernel_api/completion/test_completion.c
@@ -0,0 +1,16 @@
+#include <linux/kernel.h>
+#include <linux/module.h>
+
+static int __init demo_init(void)
+{
+    return 0;
+}
+
+static void __exit demo_exit(void)
+{
+}
+
+module_init(demo_init);
+module_exit(demo_exit);
+MODULE_AUTHOR("IotaHydrae writeforever@foxmail.com");`
+MODULE_LICENSE("GPL");
