5931f6174341e500c9c7f03be51f575c91200662
iotah <writeforever@foxmail.com>
2023-03-28 18:40:18 +0800

    program seems okay
    Signed-off-by: iotah <writeforever@foxmail.com>

--- usb/simple_mouse/usb_mouse.c
+++ usb/simple_mouse/usb_mouse.c
@@ -9,8 +9,18 @@
 #define DRV_NAME "usb_mouse"
 
 struct usb_mouse_raw {
+    struct urb                  *urb;
+    struct usb_device           *dev;
     struct usb_interface        *intf;
     const struct usb_device_id  *id;
+
+    struct input_dev            *indev;
+
+    unsigned int                pipe;
+    unsigned int                maxp;
+
+    void                        *transfer_buffer;
+    dma_addr_t                  phy_addr;
 };
 
 /*
@@ -21,15 +31,66 @@ struct usb_mouse_raw {
  *  will founded it and create a `usb_device`
  */
 
-static int usb_mouse_raw_open(struct input_dev *dev)
+/*
+ * This function used to do the real raw data handle things
+ */
+static void usb_mouse_raw_irq(struct urb *urb)
+{
+    struct input_dev        *dev  = urb->context;
+    struct usb_mouse_raw    *umr  = input_get_drvdata(dev);
+    signed char             *data = umr->transfer_buffer;
+
+    switch(urb->status) {
+    case 0:     /* success */
+        break;
+    case -ECONNRESET:
+    case -ENOENT:
+    case -ESHUTDOWN:
+        return;
+    default:
+        goto resubmit;
+    }
+
+    /* handle the raw data */
+    input_report_key(dev, KEY_L, data[0] & 0x01);   /* left key pressed */
+    input_report_key(dev, KEY_S, data[0] & 0x02);   /* right key pressed */
+    input_report_key(dev, KEY_UP, data[0] & 0x10);   /* extra key pressed */
+    input_report_key(dev, KEY_DOWN, data[0] & 0x8);   /* side key pressed */
+    
+    input_report_key(dev, KEY_ENTER, data[0] & 0x04);   /* middle key pressed */
+    input_sync(dev);
+resubmit:
+    usb_submit_urb(urb, GFP_ATOMIC);
+}
+
+static int usb_mouse_raw_indev_open(struct input_dev *dev)
 {
     /* alloc, fill, submit URB */
+    struct usb_mouse_raw *umr = input_get_drvdata(dev);
+    struct urb *urb = usb_alloc_urb(0, GFP_KERNEL);
+    umr->urb = urb;
+
+    usb_fill_bulk_urb(urb,
+                      umr->dev,
+                      umr->pipe,
+                      umr->transfer_buffer,
+                      umr->maxp > 8 ? 8 : umr->maxp,
+                      usb_mouse_raw_irq,
+                      dev);
+
+    urb->transfer_dma = umr->phy_addr;
+    urb->transfer_flags |= URB_NO_TRANSFER_DMA_MAP;
+
     return 0;
 }
 
-static void usb_mouse_raw_close(struct input_dev *dev)
+static void usb_mouse_raw_indev_close(struct input_dev *dev)
 {
     /* cancel, free URB */
+    struct usb_mouse_raw *umr = input_get_drvdata(dev);
+
+    usb_kill_urb(umr->urb);
+    usb_free_urb(umr->urb);
 }
 
 /*
@@ -37,10 +98,12 @@ static void usb_mouse_raw_close(struct input_dev *dev)
  * this function will be called sooner.
  */
 static int usb_mouse_raw_probe(struct usb_interface *intf,
-                           const struct usb_device_id *id)
+                               const struct usb_device_id *id)
 {
-    struct input_dev *input_dev;
-    struct usb_mouse_raw *umr;
+    struct input_dev                *input_dev;
+    struct usb_mouse_raw            *umr;
+    struct usb_host_interface       *interface;
+    struct usb_endpoint_descriptor  *endpoint_desc;
     /*
      * Here to do the real register operations, you need to do the
      * works list blow :
@@ -50,21 +113,36 @@ static int usb_mouse_raw_probe(struct usb_interface *intf,
     printk("a usb mouse has plugged in ...\n");
 
     input_dev = devm_input_allocate_device(&intf->dev);
-    
-    input_dev->name = "usb_mouse_raw";
-    input_dev->dev.parent = &intf->dev;
-    input_dev->open = usb_mouse_raw_open;
-    input_dev->close = usb_mouse_raw_close;
-
-    set_bit(EV_KEY, input_dev->evbit);
-    set_bit(KEY_A | KEY_L | KEY_ENTER, input_dev->keybit);
-    
-    umr = kmalloc(sizeof(*umr), GFP_KERNEL);
-    umr->intf = intf;
-    umr->id = id;
-    input_set_drvdata(input_dev, umr);
     usb_set_intfdata(intf, input_dev);
 
+    input_dev->name  = "usb_mouse_raw";
+    input_dev->dev.parent = &intf->dev;
+    input_dev->open  = usb_mouse_raw_indev_open;
+    input_dev->close = usb_mouse_raw_indev_close;
+
+    set_bit(EV_KEY, input_dev->evbit);
+    set_bit(KEY_UP | KEY_L | KEY_S | KEY_DOWN | KEY_ENTER, input_dev->keybit);
+
+    umr = kmalloc(sizeof(*umr), GFP_KERNEL);
+    umr->dev  = interface_to_usbdev(intf);
+    umr->intf = intf;
+    umr->id   = id;
+
+    /* getting pipe, maxp ... */
+    interface = intf->cur_altsetting;
+    if (interface->desc.bNumEndpoints != 1)
+        return -ENODEV;
+
+    endpoint_desc = &interface->endpoint[0].desc;
+    if (!usb_endpoint_is_int_in(endpoint_desc))
+        return -ENODEV;
+
+    umr->pipe = usb_rcvintpipe(umr->dev, endpoint_desc->bEndpointAddress);
+    umr->maxp = usb_maxpacket(umr->dev, umr->pipe, usb_pipeout(umr->pipe));
+
+    umr->transfer_buffer = usb_alloc_coherent(umr->dev, 8, GFP_ATOMIC, &umr->phy_addr);
+
+    input_set_drvdata(input_dev, umr);
 
     input_register_device(input_dev);
 
@@ -78,8 +156,11 @@ static void usb_mouse_raw_disconnect(struct usb_interface *intf)
 
     printk("a usb mouse has plugged out ...\n");
 
-    if (umr)
-        kfree(umr);
+    /* free things inside umr */
+    usb_free_urb(umr->urb);
+    usb_set_intfdata(intf, NULL);
+    usb_free_coherent(umr->dev, 8, umr->transfer_buffer, umr->phy_addr);
+    kfree(umr);
 
     input_unregister_device(input_dev);
 }
@@ -108,5 +189,5 @@ static struct usb_driver usb_mouse_raw_drv = {
 module_usb_driver(usb_mouse_raw_drv);
 
 MODULE_AUTHOR("IotaHydrae <writeforever@foxmail.com>");
-MODULE_DESCRIPTION("Simple usb mouse driver");
+MODULE_DESCRIPTION("Simple usb mouse raw data driver");
 MODULE_LICENSE("GPL");
